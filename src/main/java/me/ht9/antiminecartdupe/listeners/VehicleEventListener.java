1183ca4bb31c06f3a5e7c10a2b59b84d271eda5c
Ceptea <98782759+Ceptea@users.noreply.github.com>
2024-03-28 14:25:45 -0400

    Update VehicleEventListener.java

--- src/main/java/me/ht9/antiminecartdupe/listeners/VehicleEventListener.java
+++ src/main/java/me/ht9/antiminecartdupe/listeners/VehicleEventListener.java
@@ -8,40 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.vehicle.VehicleListener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 
-public class VehicleEventListener extends VehicleListener
+public class VehicleEventListener
 {
-    @Override
-    public void onVehicleMove(VehicleMoveEvent event)
-    {
-        Entity entity = this.getEntityById(event.getVehicle().getEntityId());
-        CraftPlayer craftPlayer = (CraftPlayer) PacketEventListener.player;
-        if (entity == PacketEventListener.minecart && PacketEventListener.listening)
-        {
-            if (this.isEntityFarFromPlayer(entity, PacketEventListener.player))
-            {
-                if (craftPlayer.getHandle().activeContainer instanceof ContainerChest)
-                {
-                    if (craftPlayer.getHandle().activeContainer.windowId == PacketEventListener.windowId)
-                    {
-                        (PacketEventListener.player).sendPacket(PacketEventListener.player, new Packet101CloseWindow(craftPlayer.getHandle().activeContainer.windowId));
-                    }
-                }
-            }
-        }
-    }
-
-    public Entity getEntityById(int entityId)
-    {
-        return Bukkit.getWorlds().stream()
-                .flatMap(world -> world.getEntities().stream())
-                .filter(entity -> entity.getEntityId() == entityId)
-                .findFirst()
-                .orElse(null);
-    }
-
-    public boolean isEntityFarFromPlayer(Entity entity, Player player)
-    {
-        double distanceSquared = entity.getLocation().distanceSquared(player.getLocation());
-        return Math.sqrt(distanceSquared) > 10.0;
-    }
 }
